c4d767829824c969766b3f4ca0927b21a5d6000c
Shannon Shen <22512825+lolipopshock@users.noreply.github.com>
2024-03-21 19:53:04 -0400

    Disable darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
+  darkMode: "false",
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
