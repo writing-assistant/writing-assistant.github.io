ff6bc82dca538a98ec3bdaeb1cf658b974b7cbbd
minalee-research <minal@microsoft.com>
2024-05-06 17:05:02 -0400

    update website w/ google form and contact info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { Button, buttonVariants } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-import { GitHubLogoIcon, EnvelopeClosedIcon, RocketIcon, ReaderIcon } from '@radix-ui/react-icons'
+import { GitHubLogoIcon, EnvelopeClosedIcon, RocketIcon, ReaderIcon, ClipboardIcon } from '@radix-ui/react-icons'
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
@@ -41,6 +41,7 @@ import next from "next";
 const TITLE = "A Design Space for Intelligent and Interactive Writing Assistants";
 
 
+const FORM_URL = "https://forms.gle/iyk5DiECGDdc9vSQA";
 const PAPER_URL = "http://arxiv.org/abs/2403.14117";
 const GITHUB_URL = "https://github.com/writing-assistant/writing-assistant.github.io";
 const BASE_PATH = "";
@@ -182,7 +183,7 @@ const Headline = () => (
       
       <p className="pb-4">To create this design space, we collaborated with researchers from a variety of disciplines, including Human-Computer Interaction (HCI), Natural Language Processing (NLP), Information Systems, and Education, and annotated 115 papers from HCI and NLP fields to understand the current landscape of writing assistants. We hope that our design space offers researchers and designers a practical tool to navigate, comprehend, and compare the various possibilities of writing assistants, and aid in the envisioning and design of new writing assistants.</p>
 
-      <p className="pb-4">Our design space is a <span className="font-bold">living artifact</span>, as it will evolve over time alongside the fields. We invite the community to contribute to this artifact by adding new papers, annotations, and discussions to track future developments in this space. Please visit <a href={GITHUB_URL} target="_blank" className="dsiiwa-link">our GitHub repository <span style={{ display: 'inline-block', transform: "translateY(0.5px)" }}><GitHubLogoIcon /></span></a> and contribute to the artifact.</p>
+      <p className="pb-4">Our design space is a <span className="font-bold">living artifact</span>, as it will evolve over time alongside the fields. We invite the community to contribute to this artifact by adding new papers, annotations, and discussions to track future developments in this space.</p>
     </div>
     
     <div className="dsiiwa-figure pt-8">
@@ -193,6 +194,16 @@ const Headline = () => (
           alt="The figure shows the integrated design space for writing assistants. It is comprised of five boxes, Ecosystem, Task, User, Interaction, and Technology. \n Ecosystem is the biggest box that surrounds the rest, and has the following items: Digital infrastructure (e.g., usability consistency, technical interoperability), Social factors (e.g., designing with stakeholders, desigining for social writing), Locale (e.g., local writing, remote writing), Access model (e.g., free and/or open-source software, commercial software), Norms and Rules (e.g., laws, conventions), and Change over time (e.g., authors, readers, writing, information environment). \n The top part inside Ecosystem box is occupied by Task box, which overlaps with User, Interaction, and Technology boxes. Task box has the following items: Writing stage (e.g., planning, drafting, revision), Writing context (e.g., academic, journalistic, technical), Purpose (e.g., expository, narrative, descriptive), Specificity (e.g., general direction, detailed requirements), and Audience (e.g., specified, implied).\n Interaction box is positioned in the middle of the User and Technology boxes, with arrows connecting to and from these boxes. Interaction has the following points: User - Steering the system (e.g., explicit, implicit, no control), User - Integrating system output (e.g., selection,  inspiration), UI - Interface paradigm (e.g., text editor, chatbot),  UI - Layout (e.g., writing area, separated, input UI), UI - Visual differentiation (e.g., formatting, location), UI - Interaction metaphor (e.g., agent, tool, hybrid), UI - Initiation (e.g., user-initiated, system-initiated), Technology - Output type (e.g., analysis, generation), Technology - Curation type (e.g., deterministic, curated options), and Technology - User Data access (e.g., input text, additional data). \n User box is on the left of the Interaction box, with the following points: Demographic profile (e.g., age, language and culture), User capabilities (e.g., writing expertise, efficiency), Relationship to system (e.g., agency, ownership, trust), and System output preferences (e.g., coherence, diversity). \n Technology box is on the left of Interaction box and below the User box, with the following points: Data - Source (e.g., experts, users), Data - Size (e.g., small, medium, large), Model - Type (e.g., rule-based, foundation models), Model - External resource access (e.g., tool, data), Learning - Problem (e.g., classification, generation), Learning - Algorithm (e.g., supervised, unsupervised), Learning - Training and adaptation (e.g., fine-tuning, prompting), Evaluation - Evaluator (e.g., automatic, machine-learned), Evaluation - Focus (e.g., linguistic quality, controllability), and Scalability (e.g., cost, latency)."
           className="block"
       />
+    </div>
+
+    <div className="pt-8 font-sans">
+      <p>
+        <ul className="list-disc pl-4">
+          <li><span className="font-bold">Want to add your paper/writing assistant to the list?</span> Please either (i) fill out <a href={FORM_URL} target="_blank" className="form-link">this Google form <span style={{ display: 'inline-block', transform: "translateY(0.5px)" }}><ClipboardIcon /></span></a> or (ii) create a pull request in <a href={GITHUB_URL} target="_blank" className="dsiiwa-link">our GitHub repository <span style={{ display: 'inline-block', transform: "translateY(0.5px)" }}><GitHubLogoIcon /></span></a></li>
+          <li><span className="font-bold">Have questions or found incorrect annotation?</span> Please email Mina Lee <a href="mailto:mnlee@uchicago.edu" className="dsiiwa-link"> <span style={{ display: 'inline-block', transform: "translateY(1px)" }}><EnvelopeClosedIcon/></span></a></li>
+          <li><span className="font-bold">Interested in contributing to the project?</span> Please visit <a href={GITHUB_URL} target="_blank" className="dsiiwa-link">our GitHub repository <span style={{ display: 'inline-block', transform: "translateY(0.5px)" }}><GitHubLogoIcon /></span></a> and start contributing!</li>
+        </ul>
+      </p>
     </div>
 
     {/* <div className="flex flex-wrap justify-start items-start align-start space-x-0">
