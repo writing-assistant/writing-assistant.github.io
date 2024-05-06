b2eb36f79d66327fba7eb19d993915715875840a
Shannon Shen <22512825+lolipopshock@users.noreply.github.com>
2024-05-06 13:28:43 -0400

    Add ga4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 
 import { cn } from "@/lib/utils";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-9CQYSNQGVD");
 
 export const metadata: Metadata = {
   title: "A Design Space for Intelligent and Interactive Writing Assistants",
